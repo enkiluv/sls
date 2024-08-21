5c0f19fc077764f45a21391f3cdce6d5dcea9541
Myung Ho Kim <81312682+enkiluv@users.noreply.github.com>
2024-08-21 16:16:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 st.set_page_config(
-    page_title='삶과영혼의비밀',
+    page_title='생활속의대자유',
     layout='wide',
     page_icon=':robot_face:',
 )
@@ -11,10 +11,8 @@ import pandas as pd
 from openai import OpenAI
 import pickle, zipfile
 import base64
-import io, os, re, random, datetime
+import io, os, re
 from scipy import spatial
-from tavily import TavilyClient
-from pathlib import Path
 from PIL import Image
 
 st.markdown("""<style>
@@ -42,7 +40,23 @@ def load_embeddings(pkz="embeddings.pkz"):
             pickle_data = io.BytesIO(pkl_file.read())
             data = pickle.load(pickle_data)
     return data
+
+# Load standard questions from quests.txt
+def load_standard_questions(file_path="quests.txt"):
+    if os.path.exists(file_path):
+        try:
+            with open(file_path, 'r', encoding='cp949') as f:
+                questions = f.readlines()
+        except:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                questions = f.readlines()
+        questions = [q.strip() for q in questions if q.strip()]
+    else:
+        questions = []
+    return questions
     
+standard_questions = load_standard_questions()
+
 # Set model and map model names to OpenAI model IDs
 EMBEDDING_MODEL = "text-embedding-3-large"
 MAX_RETRIES = 2
@@ -57,6 +71,8 @@ def init_chat(state):
             state[key] = value
     set_chat('prompt', [])
     set_chat('generated', [])
+    if 'imports_index' not in state:
+        state.imports_index = 0
     if 'openai_client' not in state:
         openai_key = os.getenv("OPENAI_API_KEY")
         if not openai_key:
@@ -88,6 +104,8 @@ def related_strings(query, embeddings, relatedness_fn, top_n):
     return list(strings[:top_n]), list(relatednesses[:top_n])
 
 def query_message(query, prevs, model, embeddings):
+    """Return a message for GPT, with relevant source texts pulled from embeddings."""
+
     strings = []
     relatednesses = []
     clues = ""
@@ -102,37 +120,17 @@ def query_message(query, prevs, model, embeddings):
     for string in strings:  # TODO: Must check if the clues are within the token-budget
         clues += string.strip() + "\n\n"
         
-    if 'use_websearch' in chat_state and chat_state['use_websearch']:
-        try:
-            tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
-            web_clues = tavily_client.search(query=query + "(최신자료순)", search_depth="advanced", include_answer=True)
-
-            # if 'answer' in web_clues:
-            #     clue = web_clues['answer'].strip().replace('~', '-')
-            #     strings.append(clue)
-            #     relatednesses.append(0.5)
-            #     clues += f"{clue}\n\n"
-            if 'results' in web_clues:
-                for i, result in enumerate(web_clues['results']):
-                    clue = result['content'].strip()
-                    strings.append(clue)
-                    relatednesses.append(0.5)
-                    clues += f"{clue}\n\n"
-        except Exception as e:
-            # st.exception(e)
-            # st.error(str(e))
-            pass
-
     chat_state.lookup = strings, relatednesses
 
-    unknown_ = "죄송합니다. 정보가 부족하여 답을 알 수 없습니다."
+    unknown_ = "그 질문에 대한 정보가 부족하여 답을 알 수 없습니다."
     if not clues:
-        return f"단서가 없으면 '{unknown_}' 라고 말해주세요.", []
+        return f"단서가 없으면 '{unknown_}' 라고 말해주세요."
     return f"""
-다음 단서들을 활용하여 답하고, 단서에 없는 내용을 지어서 말하지 마세요. 답변은 {expertise} 전문가의 용어나 문체를 적극 활용하고, 존댓말을 사용해주세요.
+####
+다음 단서들을 참조하고 {tone}하세요. 단서가 부족하면 '{unknown_}' 라고 답하고 없는 사실을 지어내지는 마세요. 답변은 {expertise} 전문가의 용어나 문체를 적극 활용하고, 공손한 말투를 사용해주세요.
 
 {clues}
-
+####
 """, strings
     
 def img_to_bytes(img_path):
@@ -163,6 +161,9 @@ def interact():
         img_html = f"<img src='data:image/png;base64,{img_to_bytes(img_path)}' style='display: block; margin-left: auto; margin-right: auto;'>"
         return img_html
 
+    for _ in range(2):
+        st.sidebar.markdown("")
+        
     if logo_image and os.path.exists(logo_image):
         image_html = img_to_html(logo_image)
         st.sidebar.markdown("<p style='text-align: center;'>"+image_html+"</p>", unsafe_allow_html=True)
@@ -170,18 +171,13 @@ def interact():
 
     init_chat(chat_state)
     
-    if st.sidebar.checkbox("웹 검색"):
-        chat_state['use_websearch'] = True
-    else:
-        chat_state['use_websearch'] = False
-    
     # Embeddings
     if 'embeddings' not in chat_state:
         with st.spinner("색인 정보를 로드하는 중..."):
             chat_state['embeddings'] = load_embeddings()
 
     # Generate a response
-    def generate_response(query):
+    def generate_response(query, show_clues=False):
         prevs = ""
         if len(chat_state.prompt) > 0:
             for i, _prompt in enumerate(chat_state.prompt):
@@ -191,21 +187,28 @@ def interact():
             context, clues = query_message(query, prevs, model, chat_state.embeddings)
                 
         if os.path.exists('addendum.txt'):
-            with open('addendum.txt', 'r', encoding='utf-8') as file:
-                content = file.read()
+            try:
+                with open('addendum.txt', 'r', encoding='cp949') as file:
+                    content = file.read()
+            except:
+                with open('addendum.txt', 'r', encoding='utf-8') as file:
+                    content = file.read()
             context += content
    
         if prevs:
-            context += f"\n\n이전에 {prevs}와 같은 질문들을 한 적이 있으니 아래 질문의 맥락을 파악하기 위해 참고만 하고 그 질문들에 대한 직접적인 답은 절대 하지 마세요.\n"
+            context += f"\n\n이전에 {prevs.strip()}과 같은 질문들을 한 적이 있으니 아래 질문의 맥락을 파악하기 위해 참고만 하고 그 질문들에 대한 직접적인 답은 절대 하지 마세요.\n"
     
-        context += f"\n\n[!!!! 이 질문에 답해주세요: {query} !!!!]"
+        summary = ""
+        if show_clues:
+            summary = "단서를 참조하여 답변을 하는 경우 2건 내외의 핵심 단서들을 (내용 / 출처) 형태로 간략하게 답변 뒤에 요약해주세요."
+        context += f"\n\n[!!!! 이 질문에 답해주세요: {query} !!!!\n\n{summary}]"
             
         user_content = f"{context}\n\n특별한 지시가 없는 한 {language} 언어로 {expertise} 전문가인척 응답하되, {tone}하세요."
         user_content = re.sub(r'\n\s*\n', '\n\n', user_content)
         print(user_content)
     
         system_content = f"당신은 {tone}하는 {expertise} {language} 원어민 전문가입니다."
-        
+
         with st.sidebar.expander("지식출처"):
             st.dataframe(pd.DataFrame({'참고정보': clues}), hide_index=True)
             
@@ -232,41 +235,60 @@ def interact():
             message_placeholder.markdown(new_answer)
     
         return new_answer
-        
+
     def message_response(text):
         # st.success(text)
         st.markdown(f"<div style='background-color: #efefef; color: black; padding: 15px; font-size: 13px;'>{text}</div><br>", unsafe_allow_html=True)
 
-    with st.container():
-        for i in range(len(chat_state.generated)):
-            st.chat_message("user").write(chat_state.prompt[i])
-            st.chat_message("assistant").write(chat_state.generated[i])
+    for i in range(len(chat_state.generated)):
+        st.chat_message("user").write(chat_state.prompt[i])
+        st.chat_message("assistant").write(chat_state.generated[i])
 
-        # A new query
-        user_input = st.chat_input("무엇을 도와드릴까요?")
-        if user_input:
-            st.chat_message("user").write(user_input)
-            retries = 1
-            while retries <= MAX_RETRIES:
-                try:
-                    generated = generate_response(user_input)
+    user_input = ""
+    if standard_questions:
+        with st.sidebar.expander("질문선택"):
+            selected_question = st.selectbox("질문을 선택하세요", standard_questions, index=None, key="select_text", label_visibility="collapsed")
+            if selected_question and selected_question != "":
+                user_input = selected_question
+
+    text_input = st.chat_input("무엇을 도와드릴까요?", key="user_input_text")
+    if text_input:
+        user_input = text_input
+
+    if user_input and user_input != "":
+        st.chat_message("user").write(user_input)
+        retries = 1
+        while retries <= MAX_RETRIES:
+            try:
+                generated = generate_response(user_input, show_clues)
+                break
+            except Exception as e:
+                error_msgs = str(e)
+                if "reduce the length of the messages" in error_msgs:
+                    st.error(error_msgs)
                     break
-                except Exception as e:
-                    error_msgs = str(e)
-                    if "reduce the length of the messages" in error_msgs:
-                        st.error(error_msgs)
-                        break
-                    else:
-                        retries = MAX_RETRIES + 2
-                        break
-            # After "while"...
-            chat_state['prompt'].append(user_input.strip())
-            if retries <= MAX_RETRIES:
-                chat_state['generated'].append(generated)
-            else:
-                chat_state['generated'].append(error_msgs)
-                if retries == MAX_RETRIES + 1:
-                    st.error("잠시 후에 다시 시도해 주세요.")
+                else:
+                    retries = MAX_RETRIES + 2
+                    break
+        # After "while"...
+        chat_state['prompt'].append(user_input.strip())
+        if retries <= MAX_RETRIES:
+            chat_state['generated'].append(generated)
+        else:
+            chat_state['generated'].append(error_msgs)
+            if retries == MAX_RETRIES + 1:
+                st.error("잠시 후에 다시 시도해 주세요.")
+                
+    if len(chat_state.generated) > 0:
+        cols = st.columns((39, 3))
+        with cols[1]:
+            def deselect_text():
+                chat_state.select_text = None
+            if st.button(":wastebasket:", help=':red[마지막 대화 삭제]', key="ok_chat_delete",
+                    on_click=deselect_text):
+                chat_state.prompt.pop()
+                chat_state.generated.pop()
+                st.rerun()
 
     # Bells and whistles
     with st.sidebar.expander("내보내기", expanded=False):
@@ -282,53 +304,51 @@ def interact():
 
         with st.container():
             file_type = st.radio(label="Format",
-                options=("As HTML", "As CSV", "As CHAT"), label_visibility="collapsed")
+                options=("As CSV", "As HTML"), label_visibility="collapsed")
             file_type = file_type.split()[-1].lower()
             def build_data(chat):
                 return (to_csv if file_type == "csv" else to_html)(chat)
             file_name = st.text_input("파일명", squeeze_spaces(subject))
             if file_name:
-                if file_type == "chat":
-                    file_path = file_name + "_" + \
-                        str(datetime.datetime.now())[5:19].replace(' ', '_') + ".chat"
-                    pickled_ = pickle.dumps(dict(chat_state), pickle.HIGHEST_PROTOCOL)
-                    st.download_button(label="확인", data=pickled_, file_name=file_path)
-                else:       # "csv" or "html"
-                    file_path = f"{file_name}.{file_type}"
-                    download = st.download_button(
-                        label="확인",
-                        data=build_data(pd.DataFrame({
-                            'Prompt': chat_state['prompt'],
-                            'Response': chat_state['generated'],
-                        })),
-                        file_name=file_path,
-                        mime=f'text/{file_type}')
+                file_path = f"{file_name}.{file_type}"
+                download = st.download_button(
+                    label="확인",
+                    data=build_data(pd.DataFrame({
+                        'Prompt': chat_state['prompt'],
+                        'Response': chat_state['generated'],
+                    })),
+                    file_name=file_path,
+                    mime=f'text/{file_type}')
 
     with st.sidebar.expander("불러오기", expanded=False):
-        conversation = st.file_uploader('대화 파일 업로드', label_visibility='collapsed')
+        conversation = st.file_uploader('대화 파일 업로드', label_visibility='collapsed',
+            key=f'chat_{chat_state.imports_index}')
         if conversation and st.button("확인",
                 key="ok_restore", help="이 메뉴를 실행하면 현재 진행중인 대화가 지워집니다!"):
             # Read the bytes of the file into a bytes object
-            file_bytes = io.BytesIO(conversation.read())
-            # Load the bytes object into a Python object using the pickle module
-            saved_chat = pickle.load(file_bytes)
+            try:
+                saved_chat = pd.read_csv(conversation, encoding="cp949")
+            except:
+                saved_chat = pd.read_csv(conversation, encoding="utf-8")
             clear_chat(chat_state)
-            chat_state['prompt'] = saved_chat['prompt']
-            chat_state['generated'] = saved_chat['generated']
+            chat_state['prompt'] = saved_chat.Prompt.tolist()
+            chat_state['generated'] = saved_chat.Response.tolist()
+            chat_state['imports_index'] += 1
             st.rerun()
 
 ###
 
-model = 'gpt-4o'
-top_n = 30
+model = 'gpt-4o-mini'
+top_n = 16
 
-expertise = '대승불교 양우종'
-tone = '완전 확실한 사실만 언급'
+expertise = '대승불교양우종'
+tone = '확실한 사실만 응답'
 temperature = 0.0
-language = '한국어'
+language = 'Korean'
 
 subject = '삶과 영혼의 비밀 + 생활 속의 대자유'
 intro = "* 대승불교 양우회 발간 <span style='color: skyblue;'>삶과 영혼의 비밀</span>과 <span style='color: orange'>생활 속의 대자유</span>의 내용에 대한 질의응답 서비스입니다.<br/>* 제공된 정보가 정확하지 않을 수 있으니, 이 정보를 참고자료로만 사용하고 필요하면 직접 더 확인해 보세요."
+show_clues = True
 
 ###
 # Launch the bot
